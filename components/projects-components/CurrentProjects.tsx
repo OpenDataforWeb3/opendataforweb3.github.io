95b57b372b3768e3e79cca3dd10625de71906574
hebbs <hebbsnator@gmail.com>
2023-09-19 02:23:31 +0300

    fix: projects icons placeholders

--- components/projects-components/CurrentProjects.tsx
+++ components/projects-components/CurrentProjects.tsx
@@ -3,6 +3,10 @@ import MediaCard from './MediaCard';
 import { Grid, Typography } from '@mui/material';
 import IconLego from '../../public/icons/IconLego';
 import IconDashboard from '../../public/icons/IconDashboard';
+import IconReview from '../../public/icons/IconReview';
+import IconBarChart from '../../public/icons/IconBarChart';
+import IconPieChart from '../../public/icons/IconPieChart';
+import IconDashboardLine from '../../public/icons/IconDashboardLine';
 
 
 export default function CurrentProjects() {
@@ -11,10 +15,10 @@ export default function CurrentProjects() {
     const content: Array<any> = [
         { title: "Sybil Scorer", link: "https://github.com/poupou-web3/sybil-scorer", src: <img src='images/sybil-scorer.svg' width={150} /> },
         { title: "Mirall0x", link: "https://github.com/OpenDataforWeb3/Mirall0x", src: <IconDashboard height={130} width={130} color={'#713996'} /> },
-        { title: " GIS ", link: "https://github.com/OpenDataforWeb3/GIS", src: <IconLego height={130} width={130} color={'#f57f02'} /> },
-        { title: "Grants Review Dashboard", link: "https://github.com/OpenDataforWeb3-sandbox/Grant-Review-Dashboard", src: <IconDashboard height={130} width={130} color={'#713996'} /> },
-        { title: "Datadex", link: "https://github.com/davidgasquez/datadex", src: <IconLego height={130} width={130} color={'#f57f02'} /> },
-        { title: "Sybil Dashboard", link: "https://github.com/OpenDataforWeb3-sandbox/sybilscan-app", src: <IconDashboard height={130} width={130} color={'#713996'} /> },
+        { title: " GIS ", link: "https://github.com/OpenDataforWeb3/GIS", src: <IconPieChart height={130} width={130} color={'#f57f02'} /> },
+        { title: "Grants Review Dashboard", link: "https://github.com/OpenDataforWeb3-sandbox/Grant-Review-Dashboard", src: <IconReview height={130} width={130} color={'#713996'} /> },
+        { title: "Datadex", link: "https://github.com/davidgasquez/datadex", src: <IconBarChart height={130} width={130} color={'#f57f02'} /> },
+        { title: "Sybil Dashboard", link: "https://github.com/OpenDataforWeb3-sandbox/sybilscan-app", src: <IconDashboardLine height={130} width={130} color={'#713996'} /> },
         { title: "Lego Curration", link: "https://github.com/OpenDataforWeb3-sandbox/Lego-Curation", src: <IconLego height={130} width={130} color={'#f57f02'} /> },
     ];
 
---
+++ public/icons/IconBarChart.tsx
@@ -0,0 +1,17 @@
+// icon:bar-chart | Radix Icons https://icons.radix-ui.com/ | WorkOS
+import * as React from "react";
+
+function IconBarChart(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg fill="none" viewBox="0 0 15 15" height="1em" width="1em" {...props}>
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M11.5 1a.5.5 0 01.5.5v12a.5.5 0 01-1 0v-12a.5.5 0 01.5-.5zm-2 2a.5.5 0 01.5.5v10a.5.5 0 01-1 0v-10a.5.5 0 01.5-.5zm4 0a.5.5 0 01.5.5v10a.5.5 0 01-1 0v-10a.5.5 0 01.5-.5zm-8 1a.5.5 0 01.5.5v9a.5.5 0 01-1 0v-9a.5.5 0 01.5-.5zm-4 1a.5.5 0 01.5.5v8a.5.5 0 01-1 0v-8a.5.5 0 01.5-.5zm6 0a.5.5 0 01.5.5v8a.5.5 0 01-1 0v-8a.5.5 0 01.5-.5zm-4 2a.5.5 0 01.5.5v6a.5.5 0 01-1 0v-6a.5.5 0 01.5-.5z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export default IconBarChart;
